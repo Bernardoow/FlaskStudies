dce05a939a053d919e7ab49d8367f6f5d5dc23d4
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-12-18 07:55:00 -0200

    Routing

--- quickstart/hello.py
+++ quickstart/hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello, World'
 
 if __name__ == "__main__":
     app.run()
