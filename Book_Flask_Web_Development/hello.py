51802193c459ebeacfbdaba8dfb9fb904bf1b843
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-12-26 12:30:58 -0200

    Application and Request Contexts

--- Book_Flask_Web_Development/hello.py
+++ Book_Flask_Web_Development/hello.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
 
-@app.route('/user/<name>')
-def user(name):
-    return "<h1>Hello, {}!</h1>".format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
