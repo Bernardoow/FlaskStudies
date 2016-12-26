4c98e7514f767272bc8aa13cf8c4f87ff0c7fdc0
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-12-26 18:22:09 -0200

    chapter 03

--- Book_Flask_Web_Development/hello.py
+++ Book_Flask_Web_Development/hello.py
@@ -1,18 +1,26 @@
-from flask import Flask, abort
+from datetime import datetime
+
+from flask import Flask, render_template
+
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return redirect("http://www.uol.com.br")
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<id>')
-def get_user(id):
-    user = None
-    if not user:
-        abort(404)
-    return '<h1>Hello, {}</h1>'.format(user)
+@app.route('/user<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
