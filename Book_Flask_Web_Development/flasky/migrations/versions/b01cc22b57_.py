1904bfcedd730c26a3c09117265892c931a20f15
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-12-29 09:06:30 -0200

    Blog Posts

---
+++ Book_Flask_Web_Development/flasky/migrations/versions/b01cc22b57_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: b01cc22b57
+Revises: 316f2287f7e
+Create Date: 2016-12-28 12:56:26.750961
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b01cc22b57'
+down_revision = '316f2287f7e'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
