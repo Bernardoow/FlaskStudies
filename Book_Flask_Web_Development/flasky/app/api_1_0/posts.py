0150c812cf373aa205305cc3d76669d4bded8b1a
Bernardo Gomes <bgomesdeabreu@gmail.com>
2016-12-30 07:29:17 -0200

    Testing

---
+++ Book_Flask_Web_Development/flasky/app/api_1_0/posts.py
@@ -0,0 +1,14 @@
+from flask import jsonify, request, g, abort, url_for, current_app
+from .. import db
+from ..models import Post, Permission
+from . import api
+from .decorators import permission_required
+from .errors import forbidden
+
+@api.route('/posts/', methods=['POST'])
+def new_post():
+    post = Post.from_json(request.json)
+    post.author = g.current_user
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.to_json())
